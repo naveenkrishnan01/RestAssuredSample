1808a14837cc455d244642b7e5036d32edbeba2e
Naveen <naveen@Naveens-MacBook-Air.local>
2020-05-11 12:47:54 -0700

    demo1

--- src/test/java/com/sample/TestCases/getWithAssertion.java
+++ src/test/java/com/sample/TestCases/getWithAssertion.java
@@ -75,7 +75,7 @@ public class getWithAssertion {
     @Test()
     public void testpoc() {
 
-        System.out.println("Welcome poc test ci-cd for Naresh ");
+        System.out.println("Welcome poc test ci-cd for demo ");
 
     }
     }
